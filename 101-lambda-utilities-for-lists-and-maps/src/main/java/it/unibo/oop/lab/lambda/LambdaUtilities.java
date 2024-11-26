7e1eced4d37705c6162457c139a5c96fc981c9d4
Nicolas Tazzieri - nicolas.tazzieri@studio.unibo.it <nicolas.tazzieri@studio.unibo.it>
2024-11-26 16:01:34 +0100

    Completed ex.1

--- 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
+++ 101-lambda-utilities-for-lists-and-maps/src/main/java/it/unibo/oop/lab/lambda/LambdaUtilities.java
@@ -2,6 +2,7 @@ package it.unibo.oop.lab.lambda;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -12,6 +13,8 @@ import java.util.function.Supplier;
 import java.util.function.UnaryOperator;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+//import javax.print.attribute.HashPrintJobAttributeSet;
 
 /**
  * This class will contain four utility functions on lists and maps, of which the first one is provided as example.
@@ -61,7 +64,9 @@ public final class LambdaUtilities {
         /*
          * Suggestion: consider Optional.filter
          */
-        return null;
+        final List<Optional<T>> l = new ArrayList<>(list.size());
+        list.forEach(elem -> l.add(Optional.of(elem).filter(pre)));
+        return l;
     }
 
     /**
@@ -80,7 +85,16 @@ public final class LambdaUtilities {
         /*
          * Suggestion: consider Map.merge
          */
-        return null;
+        final Map<R, Set<T>> m = new HashMap<>();
+        list.forEach(e -> {
+            m.merge(op.apply(e), Set.of(e), (s1, s2) -> {
+                final Set<T> s = new HashSet<>();
+                s.addAll(s1);
+                s.addAll(s2); 
+                return s;
+            });
+        });
+        return m;
     }
 
     /**
@@ -101,7 +115,9 @@ public final class LambdaUtilities {
          *
          * Keep in mind that a map can be iterated through its forEach method
          */
-        return null;
+        final Map<K, V> m = new HashMap<>();
+        map.forEach((k, v) -> m.put(k, v.orElse(def.get())));
+        return m;
     }
 
     /**
